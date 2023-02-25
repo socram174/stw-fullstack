e2614d85b072ecb5fe406802ac4dea66b976b5ee
socram174 <socram174@hotmail.com>
2023-02-24 22:30:12 -0300

    Primera version de la pagina

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [datos, setDatos] = useState([]);
+  const current = new Date();
+  const today = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
 
   useEffect(() => {
-    fetch('http://localhost:3001/alerts/update', {body: JSON.stringify({date: "02/23/2023"}), method: "POST",headers: {'Content-Type': 'application/json'
+    fetch('http://localhost:3001/alerts/update', {body: JSON.stringify({date: "02/17/2023"}), method: "POST",headers: {'Content-Type': 'application/json'
       // 'Content-Type': 'application/x-www-form-urlencoded',
 },})
       .then((res) => res.json())
@@ -20,18 +22,21 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <h1>{datos.map((dato)=> {
+      <h1>Vbucks alerts for today: {datos.length}</h1>
+      <h2>{today}</h2>
+      <div style={{backgroundColor: "lightcyan" ,color:"black"}}>
+        {datos.map((dato)=> {
           return (
-            <div>
+            <div style={{border:"5px solid green"}}>
             <h1>Zone: {dato.zone}</h1>
             <h3>vbucks: {dato.vbucks}</h3>
             <h3>date: {dato.date}</h3>
             <h3>Power Level: {dato.powerLevel}</h3>
-
             </div>
           )
-        })}</h1>
+          
+          
+        })}
       </div>
     </div>
   )
