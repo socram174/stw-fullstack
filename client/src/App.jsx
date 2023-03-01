fde0337b1716392789af5c331609f5163c20c6e2
socram174 <socram174@hotmail.com>
2023-03-01 17:08:30 -0300

    Otra posible solucion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import CssBaseline from '@mui/material/CssBaseline';
-import HomePage from './scenes/HomePage/index.jsx';
-import NavBar from "./scenes/navbar/index.jsx";
+import HomePage from './scenes/HomePage/index';
+import NavBar from "./scenes/navbar/index";
 
 function App() {
 
