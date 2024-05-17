4cf142a5b476f3d4d6e76b25dd5f3da136ca6400
socram174 <socram174@hotmail.com>
2024-05-17 00:04:25 -0400

    feat: moved the notifications section to its own component

---
+++ new-client/src/components/notifications.jsx
@@ -0,0 +1,12 @@
+const Notifications = () => {
+  return (
+    <div
+      id="notifications"
+      className="h-1/2 flex justify-center items-center w-[90%] bg-green-400 rounded-md"
+    >
+      <h1 className="text-4xl font-bold text-white">NOTIFICATIONS</h1>
+    </div>
+  );
+};
+
+export default Notifications;
