095ee1785f57f2ed129a20d74e64fe6a0083411c
socram174 <socram174@hotmail.com>
2025-02-28 22:53:54 -0300

    Added some MAILGUN envs

--- server/controllers/mail.js
+++ server/controllers/mail.js
@@ -1,3 +1,6 @@
+//import fetch from 'node-fetch';
+import fs from 'fs'
+
 export const addSubscriber = async (req, res) => {
     try {
         res.status(200).json({message: "Add new subscriber"});
@@ -17,7 +20,33 @@ export const notifySubscribers = async (req, res) => {
 export const getSubscribers = async (req, res) => {
     //const {date} = req.body;
     try {
-        res.status(200).json({message: "Get subscribers"});
+        const listAddress = process.env.MAILGUN_LIST_ADDRESS;
+        const resp = await fetch(
+            `${process.env.MAILGUN_BASE_URL}lists/${listAddress}/members`,
+            {
+            method: 'GET',
+            headers: {
+                Authorization: 'Basic ' + Buffer.from(`<username>:${process.env.MAILGUN_API_KEY}`).toString('base64')
+            }
+            }
+        );
+
+        const data = await resp.text();
+        console.log(data);
+
+        const finalData = JSON.stringify(JSON.parse(data),null,2);
+
+        // fs.writeFileSync('./hola.json',finalData,'utf-8');
+
+
+        // const parseado = JSON.parse(data)
+
+        // parseado.items.forEach(element => {
+        //     if(element.address ){
+        //         console.log(element.address);
+        //     }
+        // });
+        res.status(200).json({subs: JSON.parse(data).items});
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
